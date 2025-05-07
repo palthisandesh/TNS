630636b037699ad3cc85474112945af79b240067
navee <navee@LAPTOP-1CLES4FG>
2025-05-07 21:40:03 +0530

    change in Encapsulation

--- TNS/src/com/tns/Encapsulation/EncapsulationEx.java
+++ TNS/src/com/tns/Encapsulation/EncapsulationEx.java
@@ -1,8 +1,27 @@
 package com.tns.Encapsulation;
 
 public class EncapsulationEx {
-	public static void main(String[] args) {
-		System.out.println("mcksn");
+	private int sid;
+	private String name;
+	private double sal;
+	public int getSid() {
+		return sid;
 	}
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getSal() {
+		return sal;
+	}
+	public void setSal(double sal) {
+		this.sal = sal;
+	}
+	
 
 }
---
+++ TNS/src/com/tns/Encapsulation/EncapsulationMain.java
@@ -0,0 +1,18 @@
+package com.tns.Encapsulation;
+
+public class EncapsulationMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		EncapsulationEx e=new EncapsulationEx();
+		e.setSid(101);
+		e.setName("sandesh");
+		e.setSal(2999);
+		System.out.println(e.getSal());
+		System.out.println(e.getName());
+		System.out.println(e.getSal());
+
+	}
+
+}
